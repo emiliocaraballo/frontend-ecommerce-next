c630e4a282e38bfbefdbe377c7fffa8bed2bf254
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2024-12-18 14:56:57 -0500

    fix: store.ts

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,36 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import { combineReducers, Middleware } from 'redux';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
-import { api } from './api'; // Asegúrate de importar la configuración de RTK Query
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import { api } from './api';
 import userReducer from './slices/userSlice';
 
 // Configuración de persistencia
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['user'], // Reducers que quieres persistir
+  whitelist: ['user', 'api'],
 };
 
 // Reducer combinado
 const rootReducer = combineReducers({
   user: userReducer,
-  [api.reducerPath]: api.reducer, // Agrega el reducer de RTK Query
+  [api.reducerPath]: api.reducer,
 });
 
-// Aplica persistencia solo al reducer de `user`
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Configura el store
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false, // Deshabilita chequeo de serialización para redux-persist
-    }).concat(api.middleware), // Agrega el middleware de RTK Query
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+    
+    return middlewares.concat(api.middleware as Middleware);
+  },
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
-// Configura el persistor
 export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
